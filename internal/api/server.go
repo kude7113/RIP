1bae8861bd9635b3b516528dfa973f0b68417458
kude7113 <125797737+kude7113@users.noreply.github.com>
2024-09-24 19:45:13 +0300

    add resolution struct, add its realisation

--- internal/api/server.go
+++ internal/api/server.go
@@ -14,22 +14,32 @@ type Fine struct {
 	Title    string
 	FullInfo string
 	Price    int
+	Imge     string
+}
+
+type Resolution struct {
+	Id    int
+	Fines map[Fine]int
 }
 
 func StartServer() {
 	log.Println("Server start up")
 
 	var Fines = []Fine{
-		{Id: 1, Title: "Проезд по пешеходному переходу", FullInfo: "Как и на велосипеде, на самокате запрещено ехать по пешеходным переходам — необходимо спешиться и катить СИМ в руках.", Price: 2000},
-		{Id: 2, Title: "Езда вдвоем на электросамокате", FullInfo: "На электросамокатах запрещено перевозить пассажиров, то есть ездить вдвоем на одном транспорте нельзя.", Price: 1500},
-		{Id: 3, Title: "Передача управления электросамокатом ребенку", FullInfo: "Водить арендные самокаты можно только с 18 лет.", Price: 5000},
-		{Id: 4, Title: "Езда на самокате пьяным", FullInfo: "Запрещено передвигаться на электросамокате в нетрезвом виде.", Price: 30000},
+		{Id: 1, Title: "Проезд по пешеходному переходу", FullInfo: "Как и на велосипеде, на самокате запрещено ехать по пешеходным переходам — необходимо спешиться и катить СИМ в руках.", Price: 2000, Imge: "http://127.0.0.1:9000/fines-bucket/0.jpg"},
+		{Id: 2, Title: "Езда вдвоем на электросамокате", FullInfo: "На электросамокатах запрещено перевозить пассажиров, то есть ездить вдвоем на одном транспорте нельзя.", Price: 1500, Imge: "http://127.0.0.1:9000/fines-bucket/1.jpg"},
+		{Id: 3, Title: "Передача управления электросамокатом ребенку", FullInfo: "Водить арендные самокаты можно только с 18 лет.", Price: 5000, Imge: "http://127.0.0.1:9000/fines-bucket/2.jpg"},
+		{Id: 4, Title: "Езда на самокате пьяным", FullInfo: "Запрещено передвигаться на электросамокате в нетрезвом виде.", Price: 30000, Imge: "http://127.0.0.1:9000/fines-bucket/3.jpg"},
 	}
 
-	var Orders = []Fine{
-		Fines[0],
-		Fines[1],
-		Fines[3],
+	var Resolutions = []Resolution{
+		{
+			Id: 1,
+			Fines: map[Fine]int{
+				Fines[0]: 15,
+				Fines[1]: 16,
+			},
+		},
 	}
 
 	r := gin.Default()
@@ -38,7 +48,7 @@ func StartServer() {
 
 	r.LoadHTMLGlob("templates/*")
 
-	r.GET("/home", func(c *gin.Context) {
+	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "home.html", gin.H{
 			"fines": Fines,
 		})
@@ -72,8 +82,8 @@ func StartServer() {
 
 	})
 
-	r.GET("/home/search", func(c *gin.Context) {
-		searchText := c.Query("searchInput")
+	r.GET("/search", func(c *gin.Context) {
+		searchText := c.Query("searchFines")
 		searchText = strings.ToLower(searchText)
 
 		var filteredFines []Fine
@@ -87,13 +97,36 @@ func StartServer() {
 		// Возвращаем результаты поиска на страницу
 		c.HTML(http.StatusOK, "home.html", gin.H{
 			"fines":      filteredFines,
-			"searchText": c.Query("searchInput"),
+			"searchText": c.Query("searchFines"),
 		})
 
 	})
 
-	r.GET("/order", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "order.html", Orders)
+	r.GET("/order/:id", func(c *gin.Context) {
+		id, err := strconv.Atoi(c.Param("id"))
+
+		if err != nil {
+			c.String(http.StatusBadRequest, "Invalid ID")
+			return
+		}
+
+		var resolution *Resolution
+
+		for _, r := range Resolutions {
+			if r.Id == id {
+				resolution = &r
+				break
+			}
+		}
+
+		if resolution == nil {
+			c.String(http.StatusNotFound, "Fine not found")
+			return
+		}
+
+		c.HTML(http.StatusOK, "order.html", gin.H{
+			"resolution": resolution,
+		})
 	})
 
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
